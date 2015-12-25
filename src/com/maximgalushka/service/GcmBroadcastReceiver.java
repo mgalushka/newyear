affd6dbfa1516b912db0c2ef49b28e940fddbab6
Maxim Galushka <mgalushka@fb.com>
2015-12-25 18:30:50 +0000

    Initial commit

---
+++ src/com/maximgalushka/service/GcmBroadcastReceiver.java
@@ -0,0 +1,26 @@
+package com.maximgalushka.service;
+
+/**
+ *
+ */
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.content.WakefulBroadcastReceiver;
+import android.util.Log;
+
+public class GcmBroadcastReceiver extends WakefulBroadcastReceiver {
+  private static final String TAG = "GcmBroadcastReceiver";
+
+  @Override
+  public void onReceive(Context context, Intent intent) {
+    Log.d(TAG, "Broadcast receiver received message");
+    // Explicitly specify that GcmIntentService will handle the intent.
+    ComponentName comp = new ComponentName(context.getPackageName(),
+                                           GcmIntentService.class.getName());
+    // Start the service, keeping the device awake while it is launching.
+    startWakefulService(context, (intent.setComponent(comp)));
+    setResultCode(1);
+  }
+}
+
