65253cfeaa6d89e0202a8959351e57bd5be4f0c2
Maxim Galushka <mgalushka@fb.com>
2015-12-27 20:22:34 +0000

    test method

--- server/new_year_service.py
+++ server/new_year_service.py
@@ -105,7 +105,7 @@ class SendDebug:
 
     def POST(self):
         web.header('Access-Control-Allow-Origin', '*')
-        send_notification('Default message')
+        send_notification('Default message', 'Ukraine', 'Kiev')
         return 'OK'
 
 
